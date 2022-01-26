3143ba3224299b92b774e45e03a2f0436f6cfdaf
Gegardus <stgegard@gmail.com>
2022-01-26 18:08:55 +0400

    Fix linter errors

--- __mocks__/localStorage.js
+++ __mocks__/localStorage.js
@@ -2,15 +2,19 @@ export default class LocalStorage {
   constructor() {
     this.store = {};
   }
+
   getItem(key) {
     return this.store[key] || null;
   }
+
   setItem(key, value) {
     this.store[key] = value.toString();
   }
+
   removeItem(key) {
     delete this.store[key];
   }
+  
   reset() {
     this.store = {};
   }
