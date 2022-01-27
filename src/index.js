e9c96f264c94e72b1b041bc4c21681ee23bf05e7
Gegardus <stgegard@gmail.com>
2022-01-27 13:26:49 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,24 +5,11 @@ import { editDescription, clearCompleted, render } from './module/addRemove.js';
 
 const TodoListObj = new TodoList();
 
-export default function Starter() {
-  const threeDots = document.querySelectorAll('li');
-  threeDots.forEach((dotValue, index) => {
-    const dot = dotValue.querySelector('.dots');
-    if (dot) {
-      const newDot = dot.cloneNode(true);
-      dotValue.replaceChild(newDot, dot);
-      newDot.addEventListener('click', () => {
-        editDescription(dotValue, index, TodoListObj);
-        Starter();
-      });
-    }
-  });
-}
-
+const inputTodo = document.querySelector('.list');
 inputTodo.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    TodoListObj.addTodo();
+    TodoListObj.addTodo(inputTodo.value.trim());
+    inputTodo.value = '';
     render(TodoListObj.list, TodoListObj);
     Starter();
   }
